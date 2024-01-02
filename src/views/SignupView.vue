14746f60bbd386830bf1d83ef76d0ecb2d6e3bd4
hocine <hocine737718@gmail.com>
2024-01-02 21:09:05 +0100

    Signup Axios Commit

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -1,22 +1,26 @@
 <template>
     <section class="signup section container" id="signup">
-        <form action="" class="signup_container grid">
+        <form @submit.prevent="action_signup" class="signup_container grid">
             <h1 class="signup_title">Inscription</h1>
             <div class="signup_inputs">
                 <div class="signup_box">
-                    <input type="text" placeholder="Nom Complet" required class="signup_input" name="nom">
+                    <input v-model="nom" type="text" placeholder="Nom" required class="signup_input" name="nom">
                     <i class="ri-user-fill"></i>
                 </div>
                 <div class="signup_box">
-                    <input type="email" placeholder="Email" required class="signup_input" name="email">
+                    <input v-model="prenom" type="text" placeholder="Prénom" required class="signup_input" name="prenom">
+                    <i class="ri-user-fill"></i>
+                </div>
+                <div class="signup_box">
+                    <input v-model="email" type="email" placeholder="Email" required class="signup_input" name="email">
                     <i class="ri-mail-fill"></i>
                 </div>
                 <div class="signup_box">
-                    <input type="password" placeholder="Mot de Passe" required class="signup_input" name="motPasse">
+                    <input v-model="mdp" type="password" placeholder="Mot de Passe" required class="signup_input" name="motPasse">
                     <i class="ri-lock-2-fill"></i>
                 </div>
                 <div class="signup_box">
-                    <input type="password" placeholder="Confirmer Votre Mot de Passe" required class="signup_input" name="motPasse">
+                    <input v-model="remdp" type="password" placeholder="Confirmer Votre Mot de Passe" required class="signup_input" name="motPasse">
                     <i class="ri-lock-2-fill"></i>
                 </div>
             </div>
@@ -28,7 +32,37 @@
     </section>
 </template>
 <script>
+import axios from 'axios';
 export default {
-  name: 'SignupView'
+    name: 'SignupView',
+    props: ["baseURL"],
+    data() {
+        return {
+            nom: '',
+            prenom: '',
+            email: '',
+            mdp: '',
+            remdp: ''
+        };
+    },
+    methods:{
+        async action_signup(){
+            try {
+                const data = new URLSearchParams();
+                data.append('inscription', JSON.stringify({ nom:this.nom, prenom:this.prenom, email: this.email, mdp: this.mdp, remdp: this.remdp  }));
+                const response = await axios.post(`${this.baseURL}/signup.php`, data);
+
+                console.log(`response.data=${response.data}`);
+                console.log(`response.data[0]=${response.data[0]}`);
+                console.log(`response.data[1]=${response.data[1]}`);
+                
+                this.$router.push({ name: "login" });
+            } 
+            catch (error) {
+                // Gestion des erreurs en cas d'échec de la inscription
+                console.error("Erreur d'inscription:", error);
+            }
+        }
+    },
 }
 </script>
